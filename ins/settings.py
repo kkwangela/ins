aed57782e86908c9223625175a2beb5104a5fe6c
kewang <kew004@ucsd.edu>
2020-07-13 11:58:16 -0700

    update whitenoise

--- ins/settings.py
+++ ins/settings.py
@@ -130,3 +130,5 @@ LOGIN_REDIRECT_URL = 'posts'
 LOGOUT_REDIRECT_URL = 'posts'
 
 AUTH_USER_MODEL = 'InstaDUP.InstaUser'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
