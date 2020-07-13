9c07928f0195cb6af18cbede1f69396ea38eb35e
kewang <kew004@ucsd.edu>
2020-07-13 11:42:05 -0700

    prep for deploy

--- ins/settings.py
+++ ins/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(y^fe#(ns@sve*%w22b)-b=t7&4)!ii+q#+#+_(+48#13d03ev'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =['*']
 
 
 # Application definition
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whilenoise.runserver_nostatic'
     'django.contrib.staticfiles',
     'InstaDUP',
     'imagekit',
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ins.urls'
